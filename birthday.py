30c0d43e28cee3368368b0703d17bc4a543c9103
sth23 <47009471+sth23@users.noreply.github.com>
2019-02-08 20:59:50 -0500

    Updated from Brython Server: 2/8/2019 8:59:39 PM

--- birthday.py
+++ birthday.py
@@ -41,6 +41,7 @@ today_date = datetime.today().day
 
 today_month_name = month_name[today_month_num]
 
+# Take inputs from user
 name = input("Hello, what is your name? ")
 birth_month = input("Hi {0}, what was the name of the month you were born in? ".format(name))
 birth_year = int(input("And what year were you born in, {0}? ".format(name)))
@@ -51,4 +52,27 @@ if today_month_name.lower() == birth_month.lower():
 elif birth_month.lower() == "october" and birth_day == 31:
     print("You were born on Halloween!")
 else:
-    print("Something else")
+    # Determine birth season
+    if birth_month.lower() == "december" or birth_month.lower() == "january" or birth_month.lower() == "february":
+        season = "winter"
+    elif birth_month.lower() == "march" or birth_month.lower() == "april" or birth_month.lower() == "may":
+        season = "spring"
+    elif birth_month.lower() == "june" or birth_month.lower() == "july" or birth_month.lower() == "august":
+        season = "summer"
+    else
+        season = "fall"
+    # Determin era
+    if birth_year >= 2000:
+        era = "two thousands"
+    elif birth_year >= 1990:
+        era = "nineties"
+    elif birth_year >= 1980:
+        era = "eighties"
+    else:
+        era = "Stone Age"
+    print("{0}, you are a " + season + " baby of the " + era + ".".format(name))
+        
+        
+        
+        
+        
